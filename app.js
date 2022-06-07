77e05feca62546a7f98fb3d020a01bd2d25dedd0
Ndoh Joel <ndohjoelmbj16@gmail.com>
2022-06-07 07:24:25 +0100

    I have adjusted the cors

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "development") app.use(morgan("short"));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/users", authRoutes);
 app.use("/food", protect, foodRoutes)
