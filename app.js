bd9567a2d41a3ad0bd312e7006a940da1b2e395c
Ndoh Joel <ndohjoelmbj16@gmail.com>
2022-06-07 07:48:36 +0100

    I have adjusted the cors again

--- app.js
+++ app.js
@@ -28,10 +28,19 @@ const corsOptions ={
   optionSuccessStatus:200
 }
 
-app.use(cors(corsOptions));
 app.use(express.json());
-if (process.env.NODE_ENV === "development") app.use(morgan("short"));
 app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === "development") app.use(morgan("short"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+
+  return next()
+});
 
 app.use("/api/v1/users", authRoutes);
 app.use("/food", protect, foodRoutes)
