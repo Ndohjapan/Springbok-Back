053a6ff42c36cf1bf76b9add1a0fb991af353476
Joel Ndoh <ndohjoelmbj16@gmail.com>
2022-10-17 00:18:02 +0100

    Update fundingStatus.js

--- middleware/fundingStatus.js
+++ middleware/fundingStatus.js
@@ -5,28 +5,6 @@ const {userFeedingSchema, utilsSchema} = require("../models/mainModel")
 
 const fundingStatus = async(req, res, next) => {
     const token = req.headers["authorization"]
-    userFeedingSchema.find({}, async function(err, docs){
-        if(err){
-            return err
-        }
-        for(i=0; i<docs.length; i++){
-            try{
-                console.log(docs[i].userId)
-                let lastFundingDay = parseInt(docs[i].lastFundingDay)
-                lastFundingDay = new Date(lastFundingDay).toISOString().substring(0, 10)
-
-                await userFeedingSchema.updateMany({userId: docs[i].userId}, {$set: {lastFunding: lastFundingDay}})
-            }
-            catch(err){
-                console.log(err)
-                continue;
-            }
-
-    
-        }
-                
-        return 0
-    })
     let thirtyDaysAgo = moment().subtract(30, "days").format("YYYY-MM-DD")
     await userFeedingSchema.updateMany({lastFunding: thirtyDaysAgo}, {$set: {fundingStatus: false}})
     return next()
