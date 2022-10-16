863692501f1cbcbd6b8851d14e0361923ae90312
Joel Ndoh <ndohjoelmbj16@gmail.com>
2022-10-16 23:24:17 +0100

    Update fundingStatus.js

--- middleware/fundingStatus.js
+++ middleware/fundingStatus.js
@@ -5,21 +5,21 @@ const {userFeedingSchema, utilsSchema} = require("../models/mainModel")
 
 const fundingStatus = async(req, res, next) => {
     const token = req.headers["authorization"]
-//     userFeedingSchema.find({}, async function(err, docs){
-//         if(err){
-//             console.log(err)
-//             return err
-//         }
-//         for(i=0; i<docs.length; i++){
-//             let lastFundingDay = docs[i].lastFundingDay
-//             lastFundingDay = new Date(lastFundingDay).toISOString().substring(0, 10)
+    userFeedingSchema.find({}, async function(err, docs){
+        if(err){
+            console.log(err)
+            return err
+        }
+        for(i=0; i<docs.length; i++){
+            let lastFundingDay = docs[i].lastFundingDay
+            lastFundingDay = new Date(lastFundingDay).toISOString().substring(0, 10)
             
-//             await userFeedingSchema.updateMany({userId: docs[i].userId}, {$set: {lastFunding: lastFundingDay}})
+            await userFeedingSchema.updateMany({userId: docs[i].userId}, {$set: {lastFunding: lastFundingDay}})
     
-//         }
+        }
                 
-//         return 0
-//     })
+        return 0
+    })
     let thirtyDaysAgo = moment().subtract(30, "days").format("YYYY-MM-DD")
     await userFeedingSchema.updateMany({lastFunding: thirtyDaysAgo}, {$set: {fundingStatus: false}})
     return next()
