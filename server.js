4acd4acb109e0649bb1b4f4b4580006da0accd3f
Joel <ndohjoelmbj16@gmail.com>
2022-12-03 00:43:34 +0100

    I have made the chnages to the socket logging to console

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const io = socketio(server, {
 
 io.on("connection", async(socket) => {
   console.log(socket.id, "I Have Connected")
-  console.log(socket)
+  console.log(socket.handshake.headers.restuarant)
   if(socket.handshake.headers.restuarant){
     socket.join(socket.handshake.headers.restuarant)
   }
