b45fb4883f57f2b1ae7c3fe325683fe3b32e2da9
Joel <ndohjoelmbj16@gmail.com>
2023-04-11 19:58:37 +0100

    I have fixed the export file issue

---
+++ __test__/UploadDocumentsAndFiles.spec.js
@@ -0,0 +1,110 @@
+const app = require("../app");
+const request = require("supertest");
+const mongoose = require("mongoose");
+const FormData = require("form-data");
+const fs = require("fs");
+const path = require("path");
+
+const {
+  adminSchema,
+  userSchema,
+  tempoararyEmailSchema,
+} = require("../models/mainModel");
+const bcrypt = require("bcrypt");
+
+const en = require("../locale/en/translation");
+const { uploadFile } = require("../controllers/document");
+
+beforeEach(async () => {
+  await mongoose.connect("mongodb://localhost:27017/test1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterEach(async () => {
+  try {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+  } catch (error) {
+    return true;
+  }
+});
+
+jest.setTimeout(30000);
+
+function isURL(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return pattern.test(str);
+}
+
+const uploadProfileImage = async () => {
+  const filePath = path.resolve(__dirname, "resources", "profile2.png");
+  console.log(filePath);
+  return await request(app)
+    .post("/document/uploadDocument")
+    .attach("document", filePath)
+};
+
+describe("Upload profile picture from the app", () => {
+  it("returns 200 if file is properly uploaded", async () => {
+    const response = await uploadProfileImage();
+
+    expect(response.status).toBe(200);
+  });
+
+  it("returns status true and url field when upload is completed", async () => {
+    const response = await uploadProfileImage();
+
+    expect(response.body.status).toBeTruthy();
+    expect(response.body.url).toBeTruthy();
+  });
+
+  it("returns a real url  when upload is completed", async () => {
+    const response = await uploadProfileImage();
+
+    const url = isURL(response.body.url);
+
+    expect(url).toBeTruthy();
+  });
+});
+
+describe("Upload the compiled CSV to Cloudinary", () => {
+    it('returns status of 200 if the file is uploaded succesfully', async() => {
+
+        const filePath = "Main-Royal-Cafeteria-2023-04-04.csv";
+
+        let response = await uploadFile(filePath)
+
+        console.log(response)
+        expect(response.status).toBe(200)
+    })
+
+    it('returns successstatus true if the file is uploaded succesfully', async() => {
+
+        const filePath = "Main-Royal-Cafeteria-2023-04-04.csv";
+
+        let response = await uploadFile(filePath)
+
+        expect(response.body.success).toBe(true)
+    })
+
+    it('returns a real url  when upload is completed', async() => {
+
+        const filePath = "Main-Royal-Cafeteria-2023-04-04.csv";
+
+        let response = await uploadFile(filePath)
+
+        const url = isURL(response.body.url);
+
+        expect(url).toBeTruthy(true)
+    })
+})
--- controllers/document.js
+++ controllers/document.js
@@ -1,146 +1,141 @@
-const Formidable = require('formidable');
+const Formidable = require("formidable");
 
 const cloudinary = require("cloudinary").v2;
 const AppError = require("../utils/appError");
 
 const catchAsync = require("../utils/catchAsync");
-const {success} = require("../utils/activityLogs")
+const { success } = require("../utils/activityLogs");
 
-const dotenv = require("dotenv")
-const path = require("path")
-dotenv.config({path: "./config/config.env"})
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({ path: "./config/config.env" });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const { resolve } = require('path');
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const { resolve } = require("path");
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
 });
 
-const fileName = 'contacts.csv';
+const fileName = "contacts.csv";
 
 const uploadFile = () => {
   fs.readFile(fileName, (err, data) => {
-     if (err) throw err;
-     
+    if (err) throw err;
   });
 };
 
-exports.uploadDocument = catchAsync(async(req, res, next) => {
-    
-    const form = Formidable();
+exports.uploadDocument = catchAsync(async (req, res, next) => {
+  const form = Formidable();
 
-    form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
+    const fileName = randomFilename(
+      files.document.newFilename,
+      files.document.originalFilename
+    );
 
-        const fileName = randomFilename(files["document"].newFilename, files["document"].originalFilename)
+    try {
+      const result = await cloudinary.uploader.upload(files.document.filepath, {
+        public_id: fileName,
+        folder: "profile",
+      });
 
-        const fileStream = fs.createReadStream(path.normalize(files["document"].filepath))
+      return res.status(200).send({ status: true, url: result.secure_url });
+    } catch (error) {
+      console.log(error);
+      return next(new AppError("Error in uploading file", 500));
+    }
+  });
+});
 
-        const uploadParams = {
-            Bucket: "leadcity",
-            Body: fileStream,
-            Key: fileName
+exports.uploadFile = async (fileName) => {
+  return new Promise(async (resolve, reject) => {
+    let filePath;
+
+    if (process.env.NODE_ENV === "test") {
+        filePath = path.join(__dirname, "..", "__test__", "resources", fileName);
+    }
+    else{
+        filePath = path.join(__dirname, "..", fileName);
+    }
+
+    try {
+      const result = await cloudinary.uploader.upload(filePath, {
+        resource_type: "raw",
+        public_id: fileName,
+        folder: "transactions-summary",
+      });
+
+      resolve({ status: 200, body: { success: true, url: result.secure_url } });
+    } catch (error) {
+      console.log(error);
+      resolve({
+        status: 400,
+        body: { success: false, message: "Error in uploading file" },
+      });
+    }
+  });
+};
+
+exports.uploadBackup = async (fileName, folder) => {
+  console.log(fileName, folder);
+
+  const file = fileName;
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload(fileName, {
+        resource_type: "raw",
+        folder: folder,
+        public_id: fileName,
+      })
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+exports.getFilesInFolder = async (folder) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.api.resources(
+      { type: "upload", resource_type: "raw", prefix: folder },
+      (error, result) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          const resources = result.resources;
+          resolve(
+            resources.map(({ public_id, secure_url }) => ({
+              public_id,
+              secure_url,
+            }))
+          );
         }
+      }
+    );
+  });
+};
 
-        s3.upload(uploadParams, function(s3Err, data) {
-            if (s3Err){
-                
-                return next (new AppError(s3Err.message, 403))
-            } 
-            console.log(`File uploaded successfully at ${data}`)
-            return res.status(200).send({status: true, url: data.Location})
-    
-        });
-        
+function randomFilename(newFileName, mimetype) {
+  mimetype = mimetype.split(".");
+  let ext =
+    mimetype[mimetype.length - 1] === "*"
+      ? "png"
+      : mimetype[mimetype.length - 1];
+  ext = ext.replace(/^\W|\W*$/gm, "");
+  newFileName = `${Date.now()}.${ext}`;
 
-    });
-
-})
-
-exports.uploadFile = async(fileName) => {
-    return new Promise((resolve, reject) => {
-
-        let filePath = path.join(__dirname, "..", fileName)
-
-        const fileStream = fs.createReadStream(filePath)
-
-        const uploadParams = {
-            Bucket: "leadcity",
-            Body: fileStream,
-            Key: fileName
-        }
-
-        s3.upload(uploadParams, function(s3Err, data) {
-            if (s3Err){
-                
-                resolve( {status: 400, body: {success: false, message: s3Err.message}})
-
-            } 
-            console.log(`File uploaded successfully at ${data}`)
-            resolve( {status: 200, body: {success: true, url:data.Location}})
-    
-        });
-    })
+  return newFileName;
 }
-
-
-exports.uploadBackup = async(fileName, folder) => {
-
-    console.log(fileName, folder)
-
-    const file = fileName
-    return new Promise((resolve, reject) => {
-
-        cloudinary.uploader.upload(fileName, 
-            { 
-                resource_type: 'raw',
-                folder: folder, 
-                public_id: fileName
-            }
-        )
-        .then(result=>{
-            resolve(result)
-        })
-        .catch(err => {
-            console.log(err)
-            reject(err)
-        })
-    })
-}
-
-
-exports.getFilesInFolder = async(folder) => {
-   
-    return new Promise((resolve, reject) => {
-        cloudinary.api.resources({type: 'upload', resource_type: 'raw' , prefix: folder }, (error, result) => {
-            if (error) {
-              console.error(error);
-              reject(error)
-            } else {
-              const resources = result.resources;
-              resolve(resources.map(({ public_id, secure_url }) => ({ public_id, secure_url })))
-            }
-        });
-    })
-}
-
-function randomFilename(newFileName, mimetype){
-    mimetype = mimetype.split(".")
-    let ext = mimetype[mimetype.length - 1] === "*" ? "png" : mimetype[mimetype.length -1] 
-    ext = ext.replace(/^\W|\W*$/gm, "")
-    console.log(mimetype)
-    console.log(newFileName, ext)
-    newFileName = `${Date.now()}.${ext}`
-    console.log(newFileName)
-
-    return newFileName
-}
-  
